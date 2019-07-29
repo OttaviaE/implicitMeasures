9805ca429e1788540a2f8faf7e469cc454bb4e00
OttaviaE <otta.epifania@gmail.com>
2019-07-29 16:23:03 +0200

    Initial commit

---
+++ .Rproj.user/93C6EAD6/sources/s-90F77FCB/E357135-contents
@@ -0,0 +1,144 @@
+#' Plot SC-IATs scores
+#'
+#' Plot the scores from two different SC-IATs.
+#'
+#' @param sciat1 Dataframe with class \code{dsciat}. Contains the \emph{D} for
+#'                the first SC-IAT.
+#' @param sciat2 Dataframe with class \code{dsciat}. Contains the \emph{D} for
+#'                the second SC-IAT.
+#' @param graph String. Type of graph to display. Default is \code{density}.
+#' @param x_values Logical. Shows the values for x-axis (deafult = \code{TRUE}).
+#'                          Only for the point graph.
+#' @param gcolors String. Colors palette for plotting the results. Default is \code{dark}.
+#' @param label_sc1 String. Label to display in the graph for the first SC-IAT.
+#'                   Default is \code{SC-IAT1}.
+#' @param label_sc2 String. Label to display in the graph for the first SC-IAT.
+#'                   Default is \code{SC-IAT2}.
+#' @param label_y String. Label to plot on the y-axis.
+#' @param dens_mean Logical. Whether to include the mean in the denisity plot.
+#'                   Default is TRUE.
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples
+#' # calculate D for the SCIAT
+#'   data("raw_data") # load data
+#' sciat_data <- clean_sciat(raw_data, sbj_id = "Participant",
+#'                          block_id = "blockcode",
+#'                          latency_id = "latency",
+#'                          accuracy_id = "correct",
+#'                          block_sciat_1 = c("test.sc_dark.Darkbad",
+#'                                            "test.sc_dark.Darkgood"),
+#'                          block_sciat_2 = c("test.sc_milk.Milkbad",
+#'                                            "test.sc_milk.Milkgood"),
+#'                          trial_id  = "trialcode",
+#'                          trial_eliminate = c("reminder",
+#'                                              "reminder1"))
+#'
+#'  sciat1 <- sciat_data[[1]] # compute D for the first SC-IAT
+#'  d_sciat1 <- Dsciat(sciat1,
+#'                   mappingA = "test.sc_dark.Darkbad",
+#'                   mappingB = "test.sc_dark.Darkgood",
+#'                   non_response = "alert") # dataframe with the first D SC-IAT
+#'
+#'  sciat2 <- sciat_data[[2]] # Compute D for the second SC-IAT
+#'  d_sciat2 <- Dsciat(sciat2,
+#'                   mappingA = "test.sc_milk.Milkbad",
+#'                   mappingB = "test.sc_milk.Milkgood",
+#'                   non_response = "alert") # dataframe with the first D SC-IAT
+#'  multi_dsciat(d_sciat1, d_sciat2) # plot the D of two SC-IATs with default
+#'                                     # settings
+multi_dsciat <- function(sciat1, sciat2,
+                   graph = c("density", "violin", "point"),
+                   x_values = TRUE,
+                   gcolors = c("dark", "greens", "blues", "pinks"),
+                   label_sc1 = "SC-IAT1",
+                   label_sc2  = "SC-IAT2",
+                   label_y = "SC-IAT scores",
+                   dens_mean = T){
+  graph <- match.arg(graph)
+  gcolors <- match.arg(gcolors)
+
+  # throws an error if objects without dsciat class are passed
+  if(is.na(class(sciat1)[2]) | is.na(class(sciat2)[2])) {
+    stop("Objects must be of class dsciat")
+  }
+  if (class(sciat1)[2] != "dsciat" | class(sciat2)[2] != "dsciat") {
+    stop("Objects must be of class dsciat")
+  }
+
+
+  d_small1 <- sciat1[, c("participant", "d_sciat")]
+  d_small1$sciat <- label_sc1
+  d_small2 <- sciat2[, c("participant", "d_sciat")]
+  d_small2$sciat <- label_sc2
+  sciat_all <- rbind(d_small1, d_small2)
+  sc_mean <- aggregate(sciat_all$d_sciat,
+                       by = list(sciat_all$sciat),
+                       FUN = mean)
+  if (graph == "density"){
+    d_graph <- ggplot(sciat_all,
+                      aes(x = sciat_all$d_sciat,
+                          color = sciat_all$sciat)) +
+      geom_density(size = 1.1)  +
+      theme_minimal() + theme(axis.title.y = element_blank()) +
+      xlab(label_y)
+    if (dens_mean == T) {
+      d_graph <- d_graph + geom_vline(data=sc_mean,
+                                      aes(xintercept = sc_mean$x,
+                                          color = sc_mean$Group.1),
+                                      linetype = "dashed",
+                                      size = 0.9)
+    } else {
+      d_graph <- d_graph
+    }
+  } else if (graph == "violin"){
+    d_graph <- ggplot(sciat_all,
+                      aes(y = sciat_all$d_sciat,
+                          x = sciat_all$sciat,
+                          color = sciat_all$sciat)) +
+      geom_violin(trim = FALSE) +
+      stat_summary(fun.data=mean_sdl,
+                   geom="pointrange",
+                   color="black") +
+      theme_minimal() +
+      theme(axis.title.x = element_blank()) +
+      ylab(label_y)
+  } else if (graph == "point") {
+    d_graph <- ggplot(sciat_all,
+                      aes(x = sciat_all$participant,
+                          y = sciat_all$d_sciat,
+                          group = sciat_all$sciat)) +
+      geom_point(aes(shape = sciat_all$sciat,
+                     color = sciat_all$sciat)) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(size = 5)) +
+      #scale_colour_discrete(name  ="SC-IAT type") +
+      scale_shape_discrete(name  ="SC-IAT type") + ylab(label_y) +
+      xlab("Participant")
+    if (x_values == TRUE){
+      d_graph <- d_graph
+    } else {
+      d_graph <- d_graph + theme(axis.text.x = element_blank())
+    }
+  }
+  if (gcolors == "greens"){
+    d_graph <- d_graph + scale_color_manual(values = c("palegreen",
+                                                       "palegreen4"),
+                                            name = "SC-IAT type")
+  } else if (gcolors == "blues"){
+    d_graph <- d_graph + scale_color_manual(values = c("royalblue1",
+                                                       "royalblue4"),
+                                            name = "SC-IAT type")
+  } else if (gcolors == "pinks") {
+    d_graph <- d_graph + scale_color_manual(values = c("rosybrown1",
+                                                       "plum1"),
+                                            name = "SC-IAT type")
+  } else {
+    d_graph <- d_graph + scale_color_manual(values = c("gray74",
+                                                       "gray8"),
+                                            name = "SC-IAT type")
+  }
+  return(d_graph)
+}
---
+++ R/d_distr.R
@@ -0,0 +1,154 @@
+#' Plot IAT or SC-IAT scores (distribution)
+#'
+#' Plot the distribution of the IAT \emph{D-score} or the SC-IAT \emph{D}.
+#'
+#' @param data Dataframe with either class \code{dscore} or \code{dsciat}.
+#' @param graph String. Indicates the graphs to display. Default is
+#'                \code{histogram}
+#' @param n_bin Numeric. Indicates the number of bins to diplay.
+#' @param col_fill String. Indicates the color for filling the bars of the
+#'                  \code{histogram} or the curve of the \code{density}. Default
+#'                  is \code{royalblue}.
+#' @param col_point String. Indicates the color for the individual scores --each
+#'                   point -- in the violin plot. Default is \code{red}.
+#' @param include_stats Logical. Indicates whether to add descriptive statistics.
+#'                       The \code{mean} is depicted with a solid line. The two
+#'                       dashed lines represent +/- 2 \emph{s.d.} from the mean.
+#'                       Default is \code{FALSE}.
+#'
+#' @return A ggplot object.
+#'
+#' @export
+#'
+#' @import ggplot2
+#'
+#' @examples
+#'   # Plotting the IAT D-score
+#'   data("raw_data") # import data
+#'   iat_cleandata <- clean_iat(raw_data, sbj_id = "Participant",
+#'                           block_id = "blockcode",
+#'                           mapA_practice = "practice.iat.Milkbad",
+#'                           mapA_test = "test.iat.Milkbad",
+#'                           mapB_practice = "practice.iat.Milkgood",
+#'                           mapB_test = "test.iat.Milkgood",
+#'                           latency_id = "latency",
+#'                           accuracy_id = "correct",
+#'                           trial_id = "trialcode",
+#'                           trial_eliminate = c("reminder", "reminder1"),
+#'                           demo_id = "blockcode",
+#'                           trial_demo = "demo")
+#'   iat_data <- iat_cleandata[[1]]
+#' # calculate D-score
+#'   iat_dscore <- computeD(iat_data,
+#'                        Dscore =  "d2")
+#'   d_distr(iat_dscore) # Default graph
+#'   d_distr(iat_dscore, graph = "histogram",
+#'             n_bin = 30) # Histogram with a different number of bins
+#'   d_distr(iat_dscore, graph = "density") # IAT D-score density plot
+#'   d_distr(iat_dscore, graph = "violin") # IAT D-score violin plot
+#'
+#'   # Plot the SC-IAT D for the first SC-IAT
+#'   data("raw_data") # load data
+#'   sciat_data <- clean_sciat(raw_data, sbj_id = "Participant",
+#'                          block_id = "blockcode",
+#'                          latency_id = "latency",
+#'                          accuracy_id = "correct",
+#'                          block_sciat_1 = c("test.sc_dark.Darkbad",
+#'                                            "test.sc_dark.Darkgood"),
+#'                          block_sciat_2 = c("test.sc_milk.Milkbad",
+#'                                            "test.sc_milk.Milkgood"),
+#'                          trial_id  = "trialcode",
+#'                          trial_eliminate = c("reminder",
+#'                                              "reminder1"))
+#'
+#'  sciat1 <- sciat_data[[1]] # compute D for the first SC-IAT
+#'  d_sciat1 <- Dsciat(sciat1,
+#'                   mappingA = "test.sc_dark.Darkbad",
+#'                   mappingB = "test.sc_dark.Darkgood",
+#'                   non_response = "alert")
+#'   d_distr(d_sciat1, graph = "histogram",
+#'              include_stats = TRUE) # SC-IAT D histogram with descriptive
+#'                                    #  statistics
+
+d_distr <- function(data,
+                    graph = c("histogram", "density", "violin"),
+                    n_bin = 80,
+                    col_fill = "royalblue",
+                    col_point = "red",
+                    include_stats = FALSE){
+  graph = match.arg(graph)
+  data$variable <- 0
+  if (is.na(class(data)[2])){
+    stop("data must be an object of class dscore or dsciat")
+  } else if (class(data)[2] == "dscore"){
+    data$d <- data[, grep("dscore", colnames(data))]
+    x_lab <- "D-score"
+  } else if (class(data)[2] == "dsciat"){
+    data$d <- data[, grep("d_sciat", colnames(data))]
+    x_lab <- "D-sciat"
+  } else {
+    stop("data must be an object of class dscore or dsciat")
+  }
+
+  if (graph == "histogram"){
+    d_graph <- ggplot(data,
+                      aes(x = data$d)) +
+      geom_histogram(bins = n_bin, col = col_fill,
+                     fill = col_fill,
+                     alpha = .50)
+    d_graph <- d_graph  + theme_minimal() + xlab(x_lab)  +
+      theme(axis.title.y = element_blank())
+    if (include_stats == FALSE){
+      d_graph <- d_graph
+    } else {
+      d_graph <- d_graph + geom_vline(xintercept = mean(data$d))
+      d_graph <- d_graph + geom_vline(xintercept = (mean(data$d) +
+                                                      2*sd(data$d)),
+                                      linetype = "dotted")
+      d_graph <- d_graph + geom_vline(xintercept = (mean(data$d) -
+                                                      2*sd(data$d)),
+                                      linetype = "dotted")
+    }
+  }
+
+  else if (graph == "density"){
+    d_graph <- ggplot(data,
+                      aes(x = data$d)) +
+      geom_density(alpha = 0.70, fill = col_fill ,
+                   col = col_fill)
+    d_graph <- d_graph  + theme_minimal() + xlab(x_lab) +
+      theme(axis.title.y = element_blank())
+    if (include_stats == FALSE){
+      d_graph <- d_graph
+    } else {
+      d_graph <- d_graph + geom_vline(xintercept = mean(data$d))
+      d_graph <- d_graph + geom_vline(xintercept = (mean(data$d) +
+                                                      2*sd(data$d)),
+                                      linetype = "dotted")
+      d_graph <- d_graph + geom_vline(xintercept = (mean(data$d) -
+                                                      2*sd(data$d)),
+                                      linetype = "dotted")
+    }
+  } else if (graph == "violin"){
+    d_graph <- ggplot(data,
+                      aes(y = data$d,
+                          x = data$variable)) +
+      geom_violin(trim = F)  +
+      geom_jitter(shape=16, col = col_point,
+                  position=position_jitter(0.2))
+    d_graph <- d_graph  + theme_minimal()
+    d_graph <- d_graph + theme(axis.text.x = element_blank(),
+                               axis.title.x = element_blank()) +
+      ylab(x_lab)
+
+    if (include_stats == FALSE){
+      d_graph <- d_graph
+    } else {
+      d_graph <- d_graph + stat_summary(fun.data=mean_sdl,
+                                        geom="pointrange",
+                                        color="black")
+    }
+
+  }
+  return(d_graph)
+}
